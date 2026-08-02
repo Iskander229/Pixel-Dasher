40b0cc4e0752709598f1593d6a41501990992480
Qurt <iskander.taniev3@gmail.com>
2026-08-02 00:26:56 -0400

    Fixed wall jump depricated double jump
    finally have wall jumping from opposite walls, double jumping had to be deleted for now, adding in the next update.
    
    besides that, I added player flip sides to facing direction when moving.

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    bool isFacingRight = true; // true because by default character always looks to the right.
+
     [Header("Movement")]
     public float moveSpeed = 5f;
     float horizontalMovement;
@@ -16,11 +18,99 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Gravity")]
     public float baseGravity = 2;
-    public float maxFallSpeed = 18f;
+    public float maxFallSpeed = 15f;
     public float fallSpeedMultiplier = 1.5f;
 
-    //custom gravity for better experience
-    private void DoGravity()
+    [Header("WallMovement")]
+    public float wallSlideSpeed = 2;
+    float currentWallDir = 0; // 0-none, 1-right, -1=left
+    float lastWallDir = 0;
+
+    [Header("GroundCheck")]
+    public Transform groundCheckPos; //position on player where groundcheck starts (edited on player prefab)
+    public Vector2 groundCheckSize = new Vector2(0.1f, 0.07f); //the size of debug cube to see it lol
+    public LayerMask groundLayer; //to check if touching anything tagged as "ground"
+    bool isGrounded;
+
+
+    [Header("WallCheck")]
+    public Transform wallCheckPos;
+    public Vector2 wallCheckSize = new Vector2(0.1f, 0.65f);
+    public LayerMask wallLayer;
+
+    private void OnDrawGizmosSelected() //when selecting script owner, this method draws a debug
+    {
+        Gizmos.color = Color.red; //cube will be red
+        Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+        Gizmos.color = Color.yellow; //cube will be red
+        Gizmos.DrawWireCube(wallCheckPos.position, wallCheckSize);
+    }
+
+
+    void Start()
+    {
+        FlipPlayerSkin(); //when starting player always faces right (if default of "isFacingRight" is true
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        DoGravity();
+        DoWallSlide();
+        Debug.Log($"Current: {currentWallDir}, Last: {lastWallDir}");
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        FlipPlayerSkin(); //when applying movement (left or right) make player face that direction...
+        horizontalMovement = context.ReadValue<Vector2>().x; //movement on horizontal(x) axis
+    }
+
+    private void FlipPlayerSkin() //make player face direction of movement
+    {
+        if (isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f; //make character face the other way (in transform scale x becomes -1 or 1)
+            transform.localScale = ls;
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        CheckGrounded();
+        if (isGrounded || CheckIsWalled() && currentWallDir != 0 && currentWallDir != lastWallDir)
+        {
+            if (context.performed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+                lastWallDir = currentWallDir;
+            } 
+        }
+
+    }
+
+    private void CheckGrounded() 
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            jumpsRemaining = maxJumps; //when grounded update remaining jumps to max.
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private bool CheckIsWalled()
+    {
+        return Physics2D.OverlapBox(wallCheckPos.position, wallCheckSize, 0, wallLayer);
+        
+    }
+
+    private void DoGravity() //custom gravity for better jumping experience
     {
         if (rb.linearVelocity.y < 0) //check if Y velocity is negative (means we are falling)
         {
@@ -33,57 +123,17 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    [Header("GroundCheck")]
-    public Transform groundCheckPos; //position on player where groundcheck starts (edited on player prefab)
-    public Vector2 groundcheckSize = new Vector2(0.5f, 0.05f); //the size of debug cube to see it lol
-    public LayerMask groundLayer; //to check if touching anything tagged as "ground"
-
-    //to see our groundcheck location
-    private void OnDrawGizmosSelected() //when selecting script owner, this method draws a debug
+    private void DoWallSlide()
     {
-        Gizmos.color = Color.red; //cube will be red
-        Gizmos.DrawWireCube(groundCheckPos.position, groundcheckSize);
-    }
-
-    //to check if grounded
-    private void CheckGrounded()
-    {
-        if (Physics2D.OverlapBox(groundCheckPos.position, groundcheckSize, 0, groundLayer))
+        if (!isGrounded && CheckIsWalled() && horizontalMovement != 0)
         {
-            jumpsRemaining = maxJumps; //when grounded update remaining jumps to max.
+            currentWallDir = transform.localScale.x; //while on the wall, get if it's on left or right from player
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -wallSlideSpeed));
+        }
+        else
+        {
+            currentWallDir = 0; //not on wall
         }
     }
 
-
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
-        DoGravity();
-    }
-
-    public void Move(InputAction.CallbackContext context)
-    {
-        horizontalMovement = context.ReadValue<Vector2>().x; //movement on horizontal(x) axis
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        CheckGrounded (); //so before jumping check if we grounded (if yes remaining jumps are maxed)
-        if (jumpsRemaining > 0)
-        {
-            if (context.performed)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
-                jumpsRemaining--; //count our jumps 
-            }
-            //else if(context.canceled) {rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower/2);} - In case we want to jump half power when pressed once and fully if holding the button.
-
-        }
-
-    }
 }
