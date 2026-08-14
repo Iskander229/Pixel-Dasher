306e5220a59b48d7aee1b93f54a052f3ca1fed21
Qurt <iskander.taniev3@gmail.com>
2026-08-14 13:58:09 -0400

    Enemy Controller Refactor
    Enemy controller is now divided into Enemy base which only contains health and item stuff
    and ground enemy which should follow player on ground
    also changed flying enemy a little to use rigidbody to better handle collisions

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -5,91 +5,27 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public int maxhealth = 2;
-    private int currentHealth;
+    [Header("Stats")]
+    public int maxHealth = 2;
+    protected int currentHealth;
     public int damage = 1;
 
-    private SpriteRenderer spriteRenderer; //to make enemy flash white/red when hit
-    private Color ogColor;
+    protected SpriteRenderer spriteRenderer;
+    protected Color ogColor;
 
-    public Transform player;
-    public float chaseSpeed = 2f;
-    public float jumpForce = 2f;
-    public LayerMask groundLayer;
-
-    private Rigidbody2D rb;
-    private bool isGrounded;
-    private bool shouldJump;
-
-    //LootTable 
     [Header("Loot")]
     public List<LootItem> lootTable = new List<LootItem>();
 
-    void Start()
+    protected virtual void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        currentHealth = maxhealth;
-
         spriteRenderer = GetComponent<SpriteRenderer>();
         ogColor = spriteRenderer.color;
-
+        currentHealth = maxHealth;
     }
 
-    void Update()
+    public virtual void TakeDamage(int amount)
     {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
-        
-        //Player direction
-        float direction = Mathf.Sign(player.position.x - transform.position.x);
-
-        //player above detection
-        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, 1 << player.gameObject.layer);
-
-        if (isGrounded)
-        {
-            //chase player
-            rb.linearVelocity = new Vector2(direction * chaseSpeed, rb.linearVelocity.y);
-
-            //jump if there's gap ahead && no ground infront,
-            //else if there's player above and platform too
-
-            //if Ground
-            RaycastHit2D groundInFront = Physics2D.Raycast(transform.position, new Vector2(direction, 0), 2f, groundLayer);
-
-            //if gap
-            RaycastHit2D gapAhead = Physics2D.Raycast(transform.position + new Vector3(direction, 0, 0), Vector2.down, 2f, groundLayer);
-
-            //if platform above
-            RaycastHit2D platformAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, groundLayer);
-
-            if (!groundInFront.collider && !gapAhead.collider)
-            {
-                shouldJump = true;
-            }
-            else if(isPlayerAbove && platformAbove.collider) 
-            {
-                shouldJump = true;
-            }
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if(isGrounded && shouldJump)
-        {
-            shouldJump = false;
-            Vector2 direction = (player.position - transform.position).normalized;
-
-            Vector2 jumpDirection = direction * jumpForce;
-
-            rb.AddForce(new Vector2(jumpDirection.x, jumpForce), ForceMode2D.Impulse);
-        }
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        //flash white 
+        currentHealth -= amount;
         StartCoroutine(FlashWhite());
 
         if (currentHealth <= 0)
@@ -98,42 +34,40 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private IEnumerator FlashWhite()
+    protected IEnumerator FlashWhite()
     {
         spriteRenderer.color = Color.white;
         yield return new WaitForSeconds(0.2f);
         spriteRenderer.color = ogColor;
     }
 
-    void Die()
+    protected virtual void Die()
     {
         Debug.Log("Enemy died");
-        //Go around loottable
-        //spawn item
-        foreach(LootItem lootItem in lootTable)
+
+        foreach (LootItem lootItem in lootTable)
         {
-            if(Random.Range(0f,100f) <= lootItem.dropChance)
+            if (Random.Range(0f, 100f) <= lootItem.dropChance)
             {
-                InstantiateLoot(lootItem.ItemPrefab); //instantiate dropped loot
-                Debug.Log($"Enemy dropped: {lootItem.ItemPrefab.name} - item" );
+                InstantiateLoot(lootItem.ItemPrefab);
+                Debug.Log($"Enemy dropped: {lootItem.ItemPrefab.name} - item");
             }
             else
             {
                 Debug.Log("Enemy dropped No Items");
             }
-            break; //to drop only one item
-            
+            break; // only ever evaluates first loot entry, see note below
         }
+
         Destroy(gameObject);
     }
 
-    void InstantiateLoot(GameObject loot)
+    protected void InstantiateLoot(GameObject loot)
     {
-        if (loot) //when loot drops make instantiate it and make it red.
+        if (loot)
         {
             GameObject droppedLoot = Instantiate(loot, transform.position, Quaternion.identity);
-
-            droppedLoot.GetComponent<SpriteRenderer>().color = Color.red; 
+            droppedLoot.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
 }
--- Assets/Scripts/Enemies/FloatEnemy.cs
+++ Assets/Scripts/Enemies/FloatEnemy.cs
@@ -1,22 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FloatEnemy : MonoBehaviour
+public class FloatEnemy : EnemyController
 {
     public float floatSpeed = 2f;
     public float floatHeight = 0.5f;
-
     private Vector3 startPosition;
+    private Rigidbody2D rb;
 
-    private void Start()
+    protected override void Awake()
+    {
+        base.Awake();
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic; // no gravity/forces, but proper physics sync
+    }
+
+    void Start()
     {
         startPosition = transform.position;
     }
 
-    private void Update()
+    void FixedUpdate()
     {
         float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
-        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        rb.MovePosition(new Vector2(transform.position.x, newY));
     }
 }
